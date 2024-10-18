f463ecf731a12418e63204eb27303633ace4f6e4
simonschaluppe <simon.schneider@technikum-wien.at>
2024-10-18 18:15:11 +0200

    added config colors

---
+++ config.py
@@ -0,0 +1,28 @@
+tile_size = 100
+screen_width = 800
+screen_height = 600
+player_dark_shades = [(160, 80, 80), (80, 160, 80), (80, 80, 160), (160, 160, 80)]
+player_light_shades = [
+    (200, 100, 100),
+    (100, 200, 100),
+    (100, 100, 200),
+    (200, 200, 100),
+]
+
+
+colors = {
+    "Title": (100, 30, 0),
+    "DEBUG": (255, 0, 0),
+    "Button hovered": (61, 98, 116),
+    "Button": (51, 58, 96),
+    "UI Text": (76, 37, 29),
+    "Dialog Background": (51, 58, 96),
+    "NONE": (0, 0, 0),
+    "AGRICULTURE": (0, 150, 0),
+    "LOWRISE": (250, 150, 150),
+    "MIDRISE": (250, 100, 100),
+    "HIGHRISE": (250, 50, 50),
+    "INDUSTRY": (100, 50, 50),
+    "PUBLIC": (150, 0, 200),
+    "Grid": (0, 255, 0),
+}
--- constants.py
+++
@@ -1,5 +0,0 @@
-tile_size = 100
-screen_width = 800
-screen_height = 600
-player_dark_shades = [(160, 80, 80), (80, 160, 80), (80, 80, 160), (160, 160, 80)]
-player_light_shades = [(200, 100, 100), (100, 200, 100), (100, 100, 200), (200, 200, 100)]
